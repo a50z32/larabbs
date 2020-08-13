ad8db44920738ad67976b51561c409476568f079
vagrant <vagrant@homestead>
2020-08-13 06:03:26 +0000

    用户可以创建话题

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,7 @@ class UsersTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'ooo';
-        $user->email = '286624230@qq.com.com';
+        $user->email = '286624230@qq.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
