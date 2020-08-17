940a941001dc857007e9e86668da1389572b5e4c
vagrant <vagrant@homestead>
2020-08-17 08:44:50 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,6 +4,8 @@ namespace App\Observers;
 
 use App\Models\Reply;
 
+use App\Notifications\TopicReplied;
+
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -19,11 +21,14 @@ class ReplyObserver
     {
         $reply->content = clean($reply->content, 'user_topic_body');
     }
-    
+
     public function created(Reply $reply)
     {
         //$reply->topic->increment('reply_count', 1);
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 }
