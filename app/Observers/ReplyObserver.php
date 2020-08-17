8e01db0e44bf2f1db4024f924582ec7c13005728
vagrant <vagrant@homestead>
2020-08-17 09:40:50 +0000

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,17 +4,18 @@ namespace App\Observers;
 
 use App\Models\Reply;
 
-use App\Notifications\TopicReplied;
-
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
+use App\Notifications\TopicReplied;
+
 class ReplyObserver
 {
-
-    public function updating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+        $reply->topic->updateReplyCount();
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
@@ -22,13 +23,8 @@ class ReplyObserver
         $reply->content = clean($reply->content, 'user_topic_body');
     }
 
-    public function created(Reply $reply)
+    public function deleted(Reply $reply)
     {
-        //$reply->topic->increment('reply_count', 1);
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
-
-        // 通知话题作者有新的评论
-        $reply->topic->user->notify(new TopicReplied($reply));
+        $reply->topic->updateReplyCount();
     }
 }
