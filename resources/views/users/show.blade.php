c31773f77c0a7b550b20e5eee6c3471ba3e6ae88
a50z32 <286624230@qq.com>
2020-08-27 08:20:14 +0000

    最后活跃时间

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,10 +11,13 @@
                 <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{ $user->introduction}} </p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
+                    <hr>
+                    <h5><strong>最后活跃</strong></h5>
+                    <p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
